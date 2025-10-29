0cc702701cad7e11de11857abfd921913b692186
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-29 12:58:03 -0500

    feat(publishing): add publishable interface for publishing workflow actions.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/ValueObjects/IPublishable.cs
@@ -0,0 +1,13 @@
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Interface defining publishing workflow actions for publishable entities.
+/// </summary>
+public interface IPublishable
+{
+    void SendToEdit();
+    void SendToApproval();
+    void ApproveAndLock();
+    void Reject();
+    void ReturnToEdit();
+}
