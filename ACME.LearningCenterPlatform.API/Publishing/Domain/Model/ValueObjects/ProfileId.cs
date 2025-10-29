10ec5210dc8853ff4cad8634eae1b081eb27db2f
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-29 12:59:18 -0500

    feat(profile): add profile id value object for unique profile identification.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/ValueObjects/ProfileId.cs
@@ -0,0 +1,7 @@
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+/// <summary>
+/// Profile Identifier Value Object
+/// </summary>
+/// <param name="Id">The unique identifier for the profile</param>
+public record ProfileId(int Id);
