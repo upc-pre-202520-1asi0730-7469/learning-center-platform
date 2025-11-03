9a40345c0fe7a1f884799b0ca99e72ff8b54e3be
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-03 17:28:50 -0500

    feat(mediator): add extension methods for configuring cortex mediator services in web-application-builder.

---
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Mediator/Cortex/Configuration/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+using Cortex.Mediator.Commands;
+using Cortex.Mediator.DependencyInjection;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Infrastructure.Mediator.Cortex.Configuration.Extensions;
+
+/// <summary>
+///   Extension methods for configuring Cortex Mediator services in a WebApplicationBuilder.
+/// </summary>
+public static class WebApplicationBuilderExtensions
+{
+    /// <summary>
+    ///  Adds the Cortex Mediator middleware to the WebApplicationBuilder.
+    /// </summary>
+    /// <param name="builder">The WebApplicationBuilder to configure.</param>
+    public static void AddCortexMediatorMiddleware(this WebApplicationBuilder builder)
+    {
+        
+        // Add Mediator Injection Configuration
+        builder.Services.AddScoped(typeof(ICommandPipelineBehavior<>), typeof(LoggingCommandBehavior<>));
+
+        // Add Cortex Mediator for Event Handling
+        builder.Services.AddCortexMediator(
+            configuration: builder.Configuration,
+            handlerAssemblyMarkerTypes: [typeof(Program)], configure: options =>
+            {
+                options.AddOpenCommandPipelineBehavior(typeof(LoggingCommandBehavior<>));
+                //options.AddDefaultBehaviors();
+            });
+    }
+}
